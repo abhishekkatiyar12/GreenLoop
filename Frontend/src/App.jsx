3f5ce572cd56d8c51fe67dd20be6fa278f83e1ce
abhishekkatiyar12 <abhishek.katiyar.983932@gmail.com>
2024-11-21 01:34:10 +0530

    admin tools

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,39 +6,60 @@ import Scroll from './components/Scroll';
 import ProductGrid from './components/ProductGrid';
 import CategoryGrid from './components/Category';
 import BrandScroller from './components/BrandScroller';
-import './components/Style.css'
-import { ChakraProvider } from '@chakra-ui/react';
+import './components/Style.css';
+import { ChakraProvider, Button, Box, Icon } from '@chakra-ui/react';
+import { FaPlus, FaTools } from 'react-icons/fa';
 import React from 'react';
 
-
-
 function App() {
-
   return (
     <ChakraProvider>
       <Header />
       <Nav />
       <Scroll />
-      <div className="fixed-buttons">
+      <Box
+        position="fixed"
+        bottom="20px"
+        right="20px"
+        display="flex"
+        flexDirection="column"
+        gap="10px"
+        zIndex="1000"
+      >
         <RoleBasedComponent allowedRoles={['admin', 'employee']}>
-          <Link to='/addproduct'>
-            <button className="addproduct">+ Add Products</button>
+          <Link to="/addproduct">
+            <Button
+              leftIcon={<Icon as={FaPlus} />}
+              colorScheme="teal"
+              size="lg"
+              variant="solid"
+              _hover={{ bg: 'teal.600' }}
+              boxShadow="lg"
+            >
+              Add Products
+            </Button>
           </Link>
         </RoleBasedComponent>
         <RoleBasedComponent allowedRoles={['admin']}>
-          <Link to='/admin-dashboard'>
-            <button className="admin-tools">Admin Tools</button>
+          <Link to="/admin-dashboard">
+            <Button
+              leftIcon={<Icon as={FaTools} />}
+              colorScheme="purple"
+              size="lg"
+              variant="solid"
+              _hover={{ bg: 'purple.600' }}
+              boxShadow="lg"
+            >
+              Admin Tools
+            </Button>
           </Link>
         </RoleBasedComponent>
-      </div>
+      </Box>
       <BrandScroller />
       <CategoryGrid />
       <ProductGrid />
-     
     </ChakraProvider>
-
-  )
+  );
 }
 
-export default App
-
+export default App;
